397381698e26e1ecc50cf097d0d41de9e7fc2db7
Daniel Mikes <max8@post.cz>
2021-08-03 20:00:48 +0200

    first commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,27 +1,94 @@
+function numberStringSplitter(input) {
+  let inputString = input.toLowerCase();
+  let number = inputString.match(/[.\d\/]+/g) || ["1"];
+  let string = inputString.match(/[a-zA-Z]+/g)[0];
+
+  return [number[0], string];
+}
+
+function checkDiv(possibleFraction) {
+  let nums = possibleFraction.split("/");
+  if (nums.length > 2) {
+    return false;
+  }
+  return nums;
+}
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
+  this.getNum = function (input) {
+    let result = numberStringSplitter(input)[0];
+    let nums = checkDiv(result);
+
+    if (!nums) {
+      return undefined;
+    }
+
+    let num1 = nums[0];
+    let num2 = nums[1] || "1";
+
+    result = Number(num1) / Number(num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+      return undefined;
+    }
+
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    let result = numberStringSplitter(input)[1];
+
+    switch (result) {
+      case "kg":
+      case "lbs":
+      case "km":
+      case "mi":
+      case "l":
+      case "gal":
+        return result;
+      default:
+        return undefined;
+    }
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
+    let result = initUnit;
     
-    return result;
+    switch (result) {
+      case "kg":
+        return "lbs";
+      case "lbs":
+        return "kg";
+      case "km":
+        return "mi";
+      case "mi":
+        return "km";
+      case "l":
+        return "gal";
+      case "gal":
+        return "L";
+      default:
+        return undefined;
+    }
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    switch (unit) {
+      case "kg":
+        return "kilograms";
+      case "lbs":
+        return "pounds";
+      case "km":
+        return "kilometers";
+      case "mi":
+        return "miles";
+      case "L":
+        return "litres";
+      case "gal":
+        return "gallons";
+      default:
+        return "ignoro";
+    }
   };
   
   this.convert = function(initNum, initUnit) {
@@ -29,16 +96,37 @@ function ConvertHandler() {
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
-    return result;
+
+    switch (initUnit) {
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "l":
+        result = initNum / galToL;
+        break;
+      case "gal":
+        result = initNum * galToL;
+        break;
+      default:
+        result = "ignorant";
+        break;
+    }
+
+    return Number(result).toFixed(5);
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express     = require('express');
-const bodyParser  = require('body-parser');
+// const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
 const cors        = require('cors');
 require('dotenv').config();
@@ -16,8 +16,8 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
@@ -29,8 +29,8 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
